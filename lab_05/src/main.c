ee2a1b25bc968e8e0cba00e2944a221ce14a2284
Simeon Romanov <archeosanacritis@gmail.com>
2023-12-16 13:07:32 +0300

    return helper

--- lab_05/src/main.c
+++ lab_05/src/main.c
@@ -1,9 +1,17 @@
 #include "huffman.h"
 
+void print_help()
+{
+    fprintf(stderr,
+            "USAGE: ./huffman [encode | decode] "
+            "<input out> <output file>\n");
+}
+
 int main(int argc, char** argv)
 {
     if(argc != 4)
     {
+        print_help();
         return FAILURE;
     }
     init();
@@ -11,6 +19,8 @@ int main(int argc, char** argv)
         encode(argv[2], argv[3]);
     else if(strcmp(argv[1], "decode") == 0)
         decode(argv[2], argv[3]);
+    else
+        print_help();
     finalise();
 
     return SUCCESS;
